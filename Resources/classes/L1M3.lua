960933599c0f2e97ea07070db285705db857e258
Thomas Donofri <underground2tj90@gmail.com>
2021-03-14 11:04:58 -0400

    Create L1M3.lua

---
+++ Resources/classes/L1M3.lua
@@ -0,0 +1,13 @@
+Car3Info = ({
+    {"kboss", 1},
+    {"cKlimo", 2},
+})[math.random(1,2)]
+
+Car3Info = ({
+    {"krust_v", 1},
+    {"krust2", 2},
+})[math.random(1,2)]
+Car3Info = ({
+    {"kboss2", 1},
+    {"cKlim2", 2},
+})[math.random(1,2)]
